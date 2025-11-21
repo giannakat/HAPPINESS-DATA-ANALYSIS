9817923b5f00c19e51bd331a9ae02e5a9b581d03
giannakat <gkcarreon@gmail.com>
2025-11-21 23:20:31 +0800

    simple layout

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ with open("styles/main.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-st.write("DATA ANALYSIS PROJECT")   
-
+st.write("ANALYZING GLOBAL HAPPINESS TREND")   
+tabs = st.tabs(["Overview", "Data Process", "Others"])
 #load dataset (world-happiness-report.csv)
 df = pd.read_csv("WHR2024.csv")
 st.dataframe(df, use_container_width=True)
