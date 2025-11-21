8cb29e5c58ab298c8489fb8744776e4d2ee2ebbc
alan-hiroshima <145443822+alan-hiroshima@users.noreply.github.com>
2025-11-21 13:55:38 +0800

    imported the dataset

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 
 # styles
@@ -6,4 +7,9 @@ with open("styles/main.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-st.write("DATA ANALYSIS PROJECT")
+st.write("DATA ANALYSIS PROJECT")   
+
+#load dataset (world-happiness-report.csv)
+df = pd.read_csv("WHR2024.csv")
+st.dataframe(df, use_container_width=True)
+# st.write(df.head())   
